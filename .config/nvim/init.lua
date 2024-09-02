0b03b3f9efe063e81d2f27221d3516bc2c428702
Jamison Lahman <jamison@lahman.dev>
2024-09-02 15:13:52 -0700

    Auto-sync Mon Sep  2 03:13:52 PM PDT 2024

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -89,6 +89,7 @@ require("lazy").setup({
                 if client.server_capabilities.documentFormattingProvider then
                     vim.api.nvim_create_autocmd("BufWritePre", {
                         group = vim.api.nvim_create_augroup("Format", { clear = true }),
+                        pattern = '*.go',
                         buffer = bufnr,
                         callback = function() vim.lsp.buf.format() end
                     })
