f256a4030ce4e969346ce33d64fadca61dcd077b
Jamison Lahman <jamison@lahman.dev>
2025-05-24 19:02:41 -0700

    lint

--- swap.sh
+++ swap.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 SWAP_SIZE="4G"
 SWAP_FILE="/swapfile"
-sudo fallocate -l $SWAP_SIZE $SWAP_FILE || sudo dd if=/dev/zero of=$SWAP_FILE bs=1M count=$(($(echo $SWAP_SIZE | sed 's/G//') * 1024 ))  status=progress
+sudo fallocate -l $SWAP_SIZE $SWAP_FILE || sudo dd if=/dev/zero of=$SWAP_FILE bs=1M count=$(( ${SWAP_SIZE/G//} * 1024 ))  status=progress
 sudo chmod 600 $SWAP_FILE
 sudo mkswap $SWAP_FILE
 sudo swapon $SWAP_FILE
